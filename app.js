94ee917eae7fa4a8187bfb84f15e6e6c3fcda7cf
Noah Maddox <nmaddox1993@live.com>
2021-06-29 11:18:35 -0600

    adding the ability for node js to serve web pages

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("welcome to home page");
-});
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "dist/GreatOutdoors")));
 
 app.get("/firstroute", (req, res) => {
   res.send("welcome to the firstroute");
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/GreatOutdoors/index.html"));
 });
 
 app.listen(process.env.PORT || 8081);
