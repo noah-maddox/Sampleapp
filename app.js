2ce13ae92a47756ec793a904abc792f832c17405
Noah Maddox <nmaddox1993@live.com>
2021-06-29 11:52:51 -0600

    adding the code to allow for my node app to call angular

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const app = express();
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "dist/GreatOutdoors")));
+app.use(express.static(path.join(__dirname, "Sampleapp/dist/Sampleapp")));
 
 app.get("/firstroute", (req, res) => {
   res.send("welcome to the firstroute");
 });
 
 app.get("/index", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/GreatOutdoors/index.html"));
+  res.sendFile(path.join(__dirname, "/Sampleapp/dist/Sampleapp/index.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/Sampleapp/dist/Sampleapp/index.html"));
 });
 
 app.listen(process.env.PORT || 8081);
