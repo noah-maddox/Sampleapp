00dd6ca19e560ac09f4f4119f91ad12ad8d572e4
Noah Maddox <nmaddox1993@live.com>
2021-06-29 10:37:58 -0600

    making my index.js match tutorial

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -11,9 +9,5 @@ app.get("/firstroute", (req, res) => {
   res.send("welcome to the firstroute");
 });
 
-const port = process.env.PORT || 8081;
-
-// Start the app by listening on the default Heroku port
-app.listen(port, (req, res) => {
-  console.log(`running on port ${port}`);
-});
+app.listen(process.env.PORT || 8081);
+module.exports = app;
