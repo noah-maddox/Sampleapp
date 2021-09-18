49dbcc3a85a31eb0ac931ea1c21cd2acd400070c
Noah Maddox <nmaddox1993@live.com>
2021-09-18 14:40:09 -0600

    messing with database file to possibly fix problems

--- database.js
+++ database.js
@@ -1,18 +1,3 @@
-// const mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "daboy.com",
-//   database: "daboycom_noah",
-//   user: "daboycom_noah",
-//   password: "sdlkdjfsd&6dDS",
-// });
-
-// connection.connect(function (err) {
-//   if (err) throw err;
-// });
-
-// module.exports = connection;
-
 var mysql = require("mysql");
 
 var pool = mysql.createPool({
